b355845be369ce70e1919e541be85ea453375e20
tzaretskaya <t.zaretskaya@teraplan.ru>
2024-12-10 02:45:42 +0300

    add mongo-sharding-repl

---
+++ mongo-sharding-repl/scripts/mongo-init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+###
+# Инициализируем бд
+###
+
+docker compose exec -T mongos_router mongosh --port 27020 <<EOF
+use somedb
+for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i})
+db.helloDoc.countDocuments()
+exit()
+EOF
+
